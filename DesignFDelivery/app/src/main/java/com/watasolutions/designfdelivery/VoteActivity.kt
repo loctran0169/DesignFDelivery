b66388e4ff660fe3826dbc080f68efea12b3216d
loctran0169 <loc.tran.intership@watasolutions.com>
2021-01-30 00:54:52 +0700

    init rating

---
+++ DesignFDelivery/app/src/main/java/com/watasolutions/designfdelivery/VoteActivity.kt
@@ -0,0 +1,12 @@
+package com.watasolutions.designfdelivery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vote)
+    }
+}
